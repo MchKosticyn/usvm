4fe335aff164d54be889dfc0e38c315269885467
MchKosticyn <mishakosticyn@yandex.ru>
2024-05-16 16:13:02 +0300

    wip

--- usvm-core/src/main/kotlin/org/usvm/constraints/TypeConstraints.kt
+++ usvm-core/src/main/kotlin/org/usvm/constraints/TypeConstraints.kt
@@ -105,6 +105,13 @@ class UTypeConstraints<Type>(
     }
 
     /**
+     * Returns type of allocated concrete heap address [ref].
+     */
+    fun typeOf(ref: UConcreteHeapAddress): Type {
+        return concreteRefToType[ref]!!
+    }
+
+    /**
      * Checks that the static ref [staticRef] can be equal by reference to the symbolic ref [symbolicRef]
      * according to their types, i.e., does a [UTypeStream] for the [symbolicRef] `contain` a concrete type of the [staticRef].
      */
