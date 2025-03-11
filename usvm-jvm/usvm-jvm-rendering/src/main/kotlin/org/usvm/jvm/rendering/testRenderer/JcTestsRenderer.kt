0caaf18d5ec632d897331d11c136896fb21bb35f
MchKosticyn <mishakosticyn@yandex.ru>
2025-03-11 19:45:23 +0300

    wip

--- usvm-jvm/usvm-jvm-rendering/src/main/kotlin/org/usvm/jvm/rendering/testRenderer/JcTestsRenderer.kt
+++ usvm-jvm/usvm-jvm-rendering/src/main/kotlin/org/usvm/jvm/rendering/testRenderer/JcTestsRenderer.kt
@@ -8,14 +8,22 @@ import com.github.javaparser.ast.expr.SimpleName
 import com.github.javaparser.printer.DefaultPrettyPrinter
 import org.jacodb.api.jvm.JcClassOrInterface
 import org.jacodb.api.jvm.JcMethod
+import org.usvm.jvm.rendering.testRenderer.testTransformers.JcCallCtorTransformer
+import org.usvm.jvm.rendering.testRenderer.testTransformers.JcPrimitiveWrapperTransformer
 import org.usvm.jvm.rendering.testRenderer.testTransformers.JcTestTransformer
+import org.usvm.jvm.rendering.testTransformers.JcDeadCodeTransformer
 import org.usvm.test.api.UTest
 import java.io.File
 import java.io.PrintWriter
 import kotlin.io.path.absolutePathString
 
 class JcTestsRenderer {
-    val transformers: List<JcTestTransformer> = TODO()
+    private val transformers: List<JcTestTransformer> = listOf(
+        JcCallCtorTransformer(),
+        JcPrimitiveWrapperTransformer(),
+        JcDeadCodeTransformer()
+    )
+
     fun renderTests(tests: List<Pair<UTest, JcTestInfo>>) {
         val testClasses = mutableMapOf<JcClassOrInterface, MutableList<Pair<JcMethod, UTest>>>()
         for ((test, info) in tests) {
@@ -28,8 +36,11 @@ class JcTestsRenderer {
             val testClassName = declType.simpleName + "Tests"
             val testClassRenderer = JcTestClassRenderer(testClassName)
             for ((method, test) in testsToRender) {
+                val transformedTest = transformers.fold(test) { currentTest, transformer ->
+                    transformer.transform(currentTest)
+                }
                 val name = SimpleName(method.name + "Test")
-                val testRenderer = testClassRenderer.addTest(name)
+                val testRenderer = testClassRenderer.addTest(transformedTest, name)
                 testRenderer.render()
             }
             val renderedTestClass = testClassRenderer.render()
