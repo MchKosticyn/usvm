92cf376b906e8f86a27cfa855bbac71454df29e0
MchKosticyn <mishakosticyn@yandex.ru>
2025-04-09 15:46:45 +0300

    wip

--- usvm-core/src/main/kotlin/org/usvm/Machine.kt
+++ usvm-core/src/main/kotlin/org/usvm/Machine.kt
@@ -41,7 +41,16 @@ abstract class UMachine<State : UState<*, *, *, *, *, State>> : AutoCloseable {
                     val state = pathSelector.peek()
                     observer.onStatePeeked(state)
 
-                    val (forkedStates, stateAlive) = interpreter.step(state)
+                    val (forkedStates, stateAlive) = try {
+                        interpreter.step(state)
+                    } catch (e: Throwable) {
+                        logger.error(e) { "Step failed" }
+                        observer.onState(state, forks = emptySequence())
+                        pathSelector.remove(state)
+                        observer.onStateTerminated(state, stateReachable = false)
+                        continue
+                    }
+
                     observer.onState(state, forkedStates)
 
                     val originalStateAlive = stateAlive && !isStateTerminated(state)
