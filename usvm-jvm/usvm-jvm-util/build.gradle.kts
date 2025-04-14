f5142cd1424959f47092f18a2eac69b696033962
petrukhinandrew <89526829+petrukhinandrew@users.noreply.github.com>
2025-04-14 17:54:30 +0300

    fix: reference mismatch in JcDeadCodeTransformer (#19)

--- usvm-jvm/usvm-jvm-util/build.gradle.kts
+++ usvm-jvm/usvm-jvm-util/build.gradle.kts
@@ -6,3 +6,11 @@ dependencies {
     implementation(Libs.jacodb_api_jvm)
     implementation(Libs.jacodb_core)
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
