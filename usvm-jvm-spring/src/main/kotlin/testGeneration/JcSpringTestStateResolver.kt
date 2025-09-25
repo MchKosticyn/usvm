3299466cc4ef979a8019752e07c3e2b4fca3549f
Andrew Petrukhin <dartmol2300@gmail.com>
2025-09-25 14:32:46 +0300

    feat: heuristics for instance creation and lombok usage

--- usvm-jvm-spring/src/main/kotlin/testGeneration/JcSpringTestStateResolver.kt
+++ usvm-jvm-spring/src/main/kotlin/testGeneration/JcSpringTestStateResolver.kt
@@ -5,11 +5,19 @@ import org.jacodb.api.jvm.JcClassType
 import org.jacodb.api.jvm.JcType
 import org.jacodb.api.jvm.JcTypedField
 import org.jacodb.api.jvm.JcTypedMethod
+import org.jacodb.api.jvm.ext.constructors
+import org.jacodb.api.jvm.ext.objectType
+import org.usvm.UAddressSort
+import org.usvm.UConcreteHeapRef
+import org.usvm.UExpr
+import org.usvm.UHeapRef
 import org.usvm.api.util.JcTestStateResolver
+import org.usvm.collection.field.UFieldLValue
 import org.usvm.machine.JcContext
 import org.usvm.memory.UReadOnlyMemory
 import org.usvm.model.UModelBase
 import org.usvm.test.api.UTestAllocateMemoryCall
+import org.usvm.test.api.UTestConstructorCall
 import org.usvm.test.api.UTestExpression
 import org.usvm.test.api.spring.JcSpringTestExecutorDecoderApi
 import utils.JcConcreteTestStateResolver
@@ -38,6 +46,47 @@ class JcSpringTestStateResolver(
                 || field.annotations.any { it.name == "jakarta.persistence.GeneratedValue" }
     }
 
-    override fun allocateClassInstance(type: JcClassType) = UTestAllocateMemoryCall(type.jcClass)
+    override fun allocateClassInstance(type: JcClassType): UTestExpression {
+        val noArgsConstructor = type.constructors.singleOrNull {
+            it.isPublic && it.parameters.isEmpty()
+        }
+
+        return if (noArgsConstructor != null)
+            UTestConstructorCall(noArgsConstructor.method, listOf())
+        else
+            UTestAllocateMemoryCall(type.jcClass)
+    }
+
     fun resolvePinnedValue(value: JcPinnedValue) = resolveExpr(value.getExpr(), value.getType())
+
+    override fun resolveObject(ref: UConcreteHeapRef, heapRef: UHeapRef, type: JcClassType): UTestExpression {
+        val currentRef = if (resolveMode == ResolveMode.CURRENT) heapRef else ref
+
+        val allArgsConstructor = type.allArgsConstructorInvocationOrNull(currentRef)
+
+        return allArgsConstructor ?: super.resolveObject(ref, heapRef, type)
+    }
+
+    private fun JcClassType.allArgsConstructorInvocationOrNull(ref: UExpr<UAddressSort>): UTestExpression? {
+        if (superType != classpath.objectType)
+            return null
+
+        val constructingFields = declaredFields.filterNot { shouldIgnoreField(it) }
+        val constructingFieldsNames = constructingFields.map { it.name.lowercase() }
+
+        val ctor = constructors.singleOrNull { ctor ->
+            ctor.parameters.size == constructingFields.size &&
+                    ctor.parameters.all { param -> param.name?.let { it.lowercase() in constructingFieldsNames } ?: false
+            }
+        }?.method
+
+        if (ctor == null) return null
+
+        val fieldsToValues = constructingFields.associate { field ->
+            val lvalue = UFieldLValue(ctx.typeToSort(field.type), ref, field.field)
+            field.name to resolveLValue(lvalue, field.type)
+        }
+
+        return UTestConstructorCall(ctor, ctor.parameters.map { fieldsToValues[it.name!!]!! })
+    }
 }
