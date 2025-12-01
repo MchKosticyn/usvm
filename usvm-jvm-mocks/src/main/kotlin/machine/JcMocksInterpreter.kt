548ecb92f6b2d23f2dca810bfbd44676badf5de9
sofyak0zyreva <k81sofia@gmail.com>
2025-12-01 22:38:51 +0300

    ref: change structure of handling concrete method calls, start implementing how mocked methods are handled

--- usvm-jvm-mocks/src/main/kotlin/machine/JcMocksInterpreter.kt
+++ usvm-jvm-mocks/src/main/kotlin/machine/JcMocksInterpreter.kt
@@ -1,45 +1,25 @@
 package machine
 
 import io.ksmt.utils.asExpr
-import org.jacodb.api.jvm.JcMethod
-import org.jacodb.api.jvm.JcType
 import org.jacodb.api.jvm.cfg.JcAssignInst
-import org.jacodb.api.jvm.cfg.JcInst
 import org.jacodb.api.jvm.cfg.JcStaticCallExpr
-import org.jacodb.api.jvm.ext.toType
-import org.jacodb.api.jvm.ext.void
-import org.jacodb.impl.features.classpaths.JcUnknownMethod
 import org.usvm.UConcreteHeapRef
-import org.usvm.api.targets.JcTarget
-import org.usvm.jvm.util.toJavaClass
-import org.usvm.collections.immutable.internal.MutabilityOwnership
-import org.usvm.constraints.UPathConstraints
 import org.usvm.machine.JcApplicationGraph
 import org.usvm.machine.JcConcreteMethodCallInst
 import org.usvm.machine.JcContext
-import org.usvm.machine.JcDynamicMethodCallInst
 import org.usvm.machine.JcInterpreterObserver
 import org.usvm.machine.JcMachineOptions
-import org.usvm.machine.JcMethodApproximationResolver
-import org.usvm.machine.JcMethodCall
 import org.usvm.machine.JcMethodCallBaseInst
-import org.usvm.machine.JcVirtualMethodCallInst
 import org.usvm.machine.interpreter.JcExprResolver
 import org.usvm.machine.interpreter.JcInterpreter
 import org.usvm.machine.interpreter.JcStepScope
-import org.usvm.machine.state.JcMethodResult
-import org.usvm.machine.state.JcState
-import org.usvm.machine.state.newStmt
 import org.usvm.machine.state.skipMethodInvocationWithValue
-import org.usvm.memory.UMemory
-import org.usvm.targets.UTargetsSet
-import org.usvm.util.findMethod
-import org.usvm.api.allocateConcreteRef
 import org.usvm.collection.field.UFieldLValue
 import org.usvm.machine.mocks.mockMethod
-import org.usvm.machine.state.addNewMethodCall
 
 val mocksMap : MutableMap<UConcreteHeapRef, String> = HashMap()
+
+val mocksMethodsMap : MutableMap<Any, String> = HashMap()
 
 open class JcMocksInterpreter(
     ctx: JcContext,
@@ -58,14 +38,25 @@ open class JcMocksInterpreter(
                 val method = stmt.method
                 val methodName = method.name
                 val retStmt = stmt.returnSite
-                if (retStmt !is JcAssignInst) {throw IllegalArgumentException("state not possible, error in jacodb and mockito compatibility")}
+                if (retStmt !is JcAssignInst) { throw IllegalArgumentException("state not possible, error in jacodb and mockito compatibility") }
+
+                if (stmt.arguments.isNotEmpty()) {
+                    val refToMock = stmt.arguments[0]
+                    if (refToMock in mocksMap) {
+                        val retType = retStmt.lhv.type
+//                        val newSymbolicRef = scope.makeSymbolicRef(retType) ?: throw IllegalArgumentException( "a")
+                        scope.doWithState {
+//                            skipMethodInvocationWithValue(stmt, newSymbolicRef)
+                            mockMethod(scope, stmt, retType)
+                        }
+                        val mocksMethodInfo = mocksMap[refToMock] + "::" + methodName
+//                        mocksMethodsMap[newSymbolicRef] = mocksMethodInfo
+                        return
+                    }
+                }
+
                 val mockCall = retStmt.rhv
                 if (methodName == "mock" && mockCall is JcStaticCallExpr && mockCall.args.size == 1) {
-//                    val entryPoint = applicationGraph.entryPoints(method).singleOrNull()
-//                    if (entryPoint == null) {
-//                        mockMethod(scope, stmt, applicationGraph)
-//                        return
-//                    }
                     scope.doWithState {
                         val classRef = stmt.arguments[0].asExpr(ctx.addressSort)
                         val classRefTypeRepresentative =
@@ -75,10 +66,11 @@ open class JcMocksInterpreter(
                         val ref = memory.allocConcrete(classType)
                         skipMethodInvocationWithValue(stmt, ref)
                         val lineNumber = stmt.returnSite.lineNumber
-                        mocksMap[ref] = classType.typeName + ": " + lineNumber
+                        mocksMap[ref] = "mock:" + classType.typeName + "(line:" + lineNumber + ")"
                     }
+                    return
                 }
-                else {super.callMethod(scope, stmt, exprResolver)}
+                super.callMethod(scope, stmt, exprResolver)
             }
             else -> super.callMethod(scope, stmt, exprResolver)
         }
