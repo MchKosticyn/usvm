8dc8aa5b987e2b1c56475d9f514ece3b0756dcf9
sofyak0zyreva <k81sofia@gmail.com>
2025-12-01 22:37:18 +0300

    ref: change CalculatorTest class and method under test

--- usvm-jvm-mocks/src/test/kotlin/org/usvm/samples/CalculatorTest.kt
+++ usvm-jvm-mocks/src/test/kotlin/org/usvm/samples/CalculatorTest.kt
@@ -2,21 +2,16 @@ package org.usvm.samples
 
 import machine.JcMocksMachine
 import org.jacodb.api.jvm.JcClasspath
+import org.jacodb.api.jvm.cfg.JcReturnInst
 import org.junit.jupiter.api.Test
 import org.usvm.UMachineOptions
+import org.usvm.api.targets.JcTarget
 import org.usvm.machine.JcInterpreterObserver
 import org.usvm.machine.JcMachine
-import org.usvm.PathSelectionStrategy
-import org.usvm.PathSelectorCombinationStrategy
 import org.usvm.test.util.checkers.ignoreNumberOfAnalysisResults
-import org.usvm.samples.JavaMethodTestRunner
-import org.usvm.samples.approximations.ApproximationsTestRunner
-import org.usvm.util.Options
-import org.usvm.util.UsvmTest
-import org.usvm.util.isException
 
 
-class CalculatorTest : JavaMethodTestRunner() {
+class CalculatorTest : MocksTestRunner() {
     override fun createMachine(
         cp: JcClasspath,
         options: UMachineOptions,
@@ -28,7 +23,7 @@ class CalculatorTest : JavaMethodTestRunner() {
     @Test
     fun testCompute() {
         checkDiscoveredPropertiesWithExceptions(
-            Calculator::testCompute,
+            Calculator::compute,
             ignoreNumberOfAnalysisResults,
             { _, _, _, r -> r.getOrNull() == 0 },
 //            { _, _, _, r -> r.isException<AssertionError>() }
