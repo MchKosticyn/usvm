13299c22fa4a89b8b7232a55bf89ad9e9fae8e88
sofyak0zyreva <k81sofia@gmail.com>
2025-12-17 18:38:15 +0300

    ref: remove create machine

--- usvm-jvm-mocks/src/test/kotlin/org/usvm/samples/ServiceTest.kt
+++ usvm-jvm-mocks/src/test/kotlin/org/usvm/samples/ServiceTest.kt
@@ -11,13 +11,13 @@ import org.usvm.test.util.checkers.ignoreNumberOfAnalysisResults
 
 
 class ServiceTest : MocksTestRunner() {
-    override fun createMachine(
-        cp: JcClasspath,
-        options: UMachineOptions,
-        interpreterObserver: JcInterpreterObserver?
-    ): JcMachine {
-        return JcMocksMachine(cp, options, interpreterObserver = interpreterObserver)
-    }
+//    override fun createMachine(
+//        cp: JcClasspath,
+//        options: UMachineOptions,
+//        interpreterObserver: JcInterpreterObserver?
+//    ): JcMachine {
+//        return JcMocksMachine(cp, options, interpreterObserver = interpreterObserver)
+//    }
 
     @Test
     fun testCalc() {
