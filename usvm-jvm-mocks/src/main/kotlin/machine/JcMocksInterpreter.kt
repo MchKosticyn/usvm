39a9f480dc4c6710de91acd65788b6f00ad1debf
sofyak0zyreva <k81sofia@gmail.com>
2025-12-25 21:49:24 +0300

    style: apply formatting

--- usvm-jvm-mocks/src/main/kotlin/machine/JcMocksInterpreter.kt
+++ usvm-jvm-mocks/src/main/kotlin/machine/JcMocksInterpreter.kt
@@ -29,14 +29,17 @@ import java.io.File
  * mocksMap tracks addresses assigned to mock objects during initialization and its metadata.
  */
 val mocksMap: MutableMap<UConcreteHeapRef, String> = HashMap()
+
 /**
  * mockedMethods simply tracks which methods were called by mock objects.
  */
 val mockedMethods: MutableSet<JcMockedMethodsValue<USort>> = mutableSetOf()
+
 /**
  * mockedMethodsValues is a map that contains methods from memory and their current return values.
  */
 val mockedMethodsValues: MutableMap<JcMockedMethodsValue<USort>, UExpr<USort>> = HashMap()
+
 /**
  * varnamesMap saves variable names and lines where they were defined.
  */
